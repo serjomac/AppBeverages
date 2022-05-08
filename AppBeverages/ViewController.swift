e246cb03f709433a10a28eb0217068f7326adbec
Sergio Macias Lopez <serjomac@espol.edu.ec>
2022-05-07 20:08:07 -0500

    Initial Commit

---
+++ AppBeverages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBeverages
+//
+//  Created by Jonathan Macias on 7/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
